fb55a6b47c18c0c1482edb557e379376fde0b180
Nakaw <nakawyoung@gmail.com>
2022-02-28 18:07:01 -0500

    Added tests for page limits and paging info file

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers {
     public class HomeController: Controller {
         private IStoreRepository repository;
+        public int PageSize = 4;
         public HomeController(IStoreRepository repo){
             repository=repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        public IActionResult Index(int productPage = 1)
+         => View(repository.Products.OrderBy(p=>p.ProductID)
+         .Skip((productPage-1) * PageSize)
+         .Take(PageSize));
     }
 }
