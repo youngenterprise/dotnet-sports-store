e272cfd62c4dca7faf801d8d876a745081d0f950
Nakaw <nakawyoung@gmail.com>
2022-03-02 11:38:49 -0500

    Added styling and restructured the page

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -40,6 +40,8 @@ namespace SportsStore
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}",
+                    new{Controller = "Home", action = "Index"});
                 endpoints.MapDefaultControllerRoute();
             });
             SeedData.EnsurePopulated(app);
