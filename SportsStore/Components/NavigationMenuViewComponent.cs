ed07ef266bc1fd0d3292d3401e769a6e02be541c
Nakaw <nakawyoung@gmail.com>
2022-03-04 16:01:52 -0500

    Passing selected category in navigation menu

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -2,16 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using SportsStore.Models;
 namespace SportsStore.Components {
- public class NavigationMenuViewComponent : ViewComponent {
- private IStoreRepository repository;
- public NavigationMenuViewComponent(IStoreRepository repo) {
- repository = repo;
- }
- public IViewComponentResult Invoke() {
- return View(repository.Products
- .Select(x => x.Category)
- .Distinct()
- .OrderBy(x => x));
- }
- }
+    public class NavigationMenuViewComponent : ViewComponent {
+    private IStoreRepository repository;
+    public NavigationMenuViewComponent(IStoreRepository repo) {
+        repository = repo;
+    }
+    public IViewComponentResult Invoke() {
+        ViewBag.SelectedCategory= RouteData?.Values["category"];
+        return View(repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x));
+            }
+        }
 }
