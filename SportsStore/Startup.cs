b85663ae896d9d59e86a4a6a6ff9329fe7961d9a
Nakaw <nakawyoung@gmail.com>
2022-03-04 11:16:03 -0500

    Added navigation menu component and unit test for the component

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -40,8 +40,18 @@ namespace SportsStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}",
+                endpoints.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
                     new{Controller = "Home", action = "Index"});
+                
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                    new{Controller = "Home", action = "Index", productPage = 1});
+
+                endpoints.MapControllerRoute("category", "{category}",
+                    new{Controller = "Home", action = "Index", productPage = 1});
+
+                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}",
+                    new{Controller = "Home", action = "Index", productPage = 1});
+                    
                 endpoints.MapDefaultControllerRoute();
             });
             SeedData.EnsurePopulated(app);
