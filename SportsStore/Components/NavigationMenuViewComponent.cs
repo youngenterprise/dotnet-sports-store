7d55209f5fef8cffb6a9181f014512e85e0d6964
Nakaw <nakawyoung@gmail.com>
2022-03-04 13:45:46 -0500

    categories showing up in view

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using SportsStore.Models;
-
-
-namespace SportsStore.Components{
-    public class NavigationMenuViewComponent:ViewComponent{
-        private IStoreRepository repository;
-        public NavigationMenuViewComponent(IStoreRepository repo){
-            repository=repo;
-        }
-        public IViewComponentResult Invoke(){
-            return View(repository.Products.Select(x=>x.Category)
-                .Distinct().OrderBy(x=>x));
-        }
-    }
+namespace SportsStore.Components {
+ public class NavigationMenuViewComponent : ViewComponent {
+ private IStoreRepository repository;
+ public NavigationMenuViewComponent(IStoreRepository repo) {
+ repository = repo;
+ }
+ public IViewComponentResult Invoke() {
+ return View(repository.Products
+ .Select(x => x.Category)
+ .Distinct()
+ .OrderBy(x => x));
+ }
+ }
 }
