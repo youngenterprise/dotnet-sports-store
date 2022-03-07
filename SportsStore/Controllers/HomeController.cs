a444d9f232a2a793531f0c97acc2d7bdc550f326
Nakaw <nakawyoung@gmail.com>
2022-03-07 12:02:34 -0500

    Changed page count to reflect amount of products in a specific category and made a unit test for this capability

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -18,8 +18,9 @@ namespace SportsStore.Controllers {
             PagingInfo = new PagingInfo {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
-            }
+                TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e=>e.Category == category).Count()
+            },
+            CurrentCategory = category
         });
     }
 }
