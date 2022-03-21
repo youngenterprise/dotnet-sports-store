45ba701b0005a89494776bc8b328bfdf6691517a
Nakaw <nakawyoung@gmail.com>
2022-03-21 11:23:00 -0400

    Added checkout page and adding orders to database with migration

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers{
+    public class OrderController: Controller{
+        public ViewResult Checkout() => View(new Order());
+    }
+}
---
+++ SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,7 @@
+using System.Linq'
+namespace SportsStore.Models{
+    public interface IOrderRepository{
+        IQueryable<Order> Orders{get;}
+        void SaveOrder(Order order);
+    }
+}
