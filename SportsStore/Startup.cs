580eae003dc8555b1b3e7fb621661bb607e0f420
Nakaw <nakawyoung@gmail.com>
2022-04-19 15:57:38 -0400

    Added account pages and content

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -42,8 +42,13 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
+            if(env.IsProduction()){
+                app.UseExceptionHandler("/error");
+            } else{
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            } 
+
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
